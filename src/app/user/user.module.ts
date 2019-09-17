12bc63c30fda93151e425b509768d809b5ab5804
Esteban Sanchez <41240399+esanchezvz@users.noreply.github.com>
2019-09-17 00:53:31 -0500

    added email login, pasword reset, and user registration

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 // Components
 import { LoginPageComponent } from './login-page/login-page.component';
 import { GoogleSigninDirective } from './google-signin.directive';
+import { EmailLoginComponent } from './email-login/email-login.component';
 
-const components = [LoginPageComponent];
-const modules = [CommonModule, UserRoutingModule, SharedModule];
+const components = [LoginPageComponent, EmailLoginComponent];
+const modules = [
+  CommonModule,
+  UserRoutingModule,
+  SharedModule,
+  ReactiveFormsModule,
+];
 
 @NgModule({
   declarations: [...components, GoogleSigninDirective],
